7f7a47fe00a9ee015f4c7777db5d0fbac8f71823
David Sheehy <ds@schwey.io>
2015-06-04 20:12:46 -0500

    Created a very redumentary bot that occaisonally posts.

---
+++ catbot/catbot.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# config, the access key and secret, the computer key and secret, and the time
+# between running.
+from config import  ACCESS_KEY, ACCESS_SECRET,CONSUMER_KEY, CONSUMER_SECRET, TIME_DELAY
+from twitter import *
+import random
+
+# various constants
+PURR = "purr"
+RESPONSES_GOOD = ['meow', 'Nya', 'MEOW', '=^.^=', PURR]
+RESPONSES_BAD = ['HISS!', '*scratch*']
+
+# open the account
+t = Twitter(auth=OAuth(ACCESS_KEY,ACCESS_SECRET,CONSUMER_KEY,CONSUMER_SECRET))
+
+# get recent messages
+
+# print a status message
+t.status.update(random.choice(RESPONSES_GOOD))
+# go back to sleep
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 A twitter bot that emulates the behavior of a cat. That is to say it sleeps,
 meows, purrs and murders small animals. The only behavior that it does not
 emulate is sitting on keyboards.
+
+config.py needs to be set up by the end user.
+I've set this program up to be run from a cron job.
